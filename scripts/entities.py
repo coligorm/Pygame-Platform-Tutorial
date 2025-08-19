6fc6ac0db3f611b9f4701c91aec224e035b7fef3
coligorm <colinrhysgorman@gmail.com>
2025-08-19 18:02:06 +1000

    Added comments

--- scripts/entities.py
+++ scripts/entities.py
@@ -4,11 +4,12 @@ class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
         self.game = game
         self.type = e_type
-        self.pos = list(pos) # 
+        self.pos = list(pos) # Converting iterable into list, each entity will have its own list
         self.size = size
         self.velocity = [0,0]
 
     def update(self, movement=(0,0)):
+        # Creating a vector to represent movement, so if gravity is being applied it is taken into account when moving up and down
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
         self.pos[0] += frame_movement[0]
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,14 @@
 import pygame
+import os
 
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img = pygame.image.load(BASE_IMG_PATH + path).convert() # convert internalised reference of the image for performance
     img.set_colorkey((0,0,0))
     return img
+
+def load_multiple_images(path):
+    images = []
+    for img_name in os.listdir(BASE_IMG_PATH + path):
+        images.append(load_image(path + '/' + img_name))
