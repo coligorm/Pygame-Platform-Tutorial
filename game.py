f1021c0ff05137e5903045e9b63b1b87ab7eda96
coligorm <colinrhysgorman@gmail.com>
2025-08-21 16:13:41 +1000

    Added the abilty to jump

--- game.py
+++ game.py
@@ -75,14 +75,16 @@ class Game:
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                        self.movement[0] = True
-                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         self.movement[1] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[0] = True
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                        self.movement[0] = False
-                    if event.key == pygame.K_RIGHT or event.key ==   pygame.K_d:
                         self.movement[1] = False
+                    if event.key == pygame.K_RIGHT or event.key ==   pygame.K_d:
+                        self.movement[0] = False
             
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
             pygame.display.update()
